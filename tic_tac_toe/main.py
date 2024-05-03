95881bcee48e1cd70b3dc72118e259c718c175f2
FeelsGo-od <andrew.sokolowsky@gmail.com>
2024-05-03 11:10:40 +0200

    add delay in tic_tac_toe project

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 board = [
@@ -7,14 +8,14 @@ board = [
     [' ', ' ', ' ']
 ]
 
-def display_board():
+def display_board(highlight=None):
     print("   0   1   2")
-
     print("  " + "---" * 3)
 
     for i, row in enumerate(board):
         print(f"{i} | {' | '.join(row)} |")
         print("  " + "---" * 3)
+        
 
 def make_move(player):
     while True:
@@ -23,42 +24,45 @@ def make_move(player):
             col = int(input(f"Player {player}, enter the column number (0, 1, or 2): "))
             if 0 <= row <= 2 and 0 <= col <= 2 and board[row][col] == ' ':
                 board[row][col] = player
-                break
+                time.sleep(0.5)
+                return
             else:
                 print('Invalid move. Please choose an empty cell within the range.')
         except ValueError:
             print('Invalid input. Please enter a number')
 
+def make_ai_move():
+    time.sleep(1) # delay to simulate AI "thinking" time
+    while True:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+
+        if board[row][col] == ' ':
+            board[row][col] = 'O'
+            print("AI has made its move:")
+            time.sleep(0.5) # delay before displaying the board
+            return
+
+
 def check_winner():
-    for row in board:
+    for i, row in enumerate(board):
         if row[0] == row[1] == row[2] != ' ':
-            return row[0]
+            return (i, 0), (i, 1), (i, 2)
     
     for col in range(3):
         if board[0][col] == board[1][col] == board[2][col] != ' ':
-            return board[0][col]
+            return (0, col), (1, col), (2, col)
     
     # Check diagonals
     if board[0][0] == board[1][1] == board[2][2] != ' ':
-        return board[0][0]
+        return (0, 0), (1, 1), (2, 2)
     if board[0][2] == board[1][1] == board[2][0] != ' ':
-        return board[0][2]
+        return (0, 2), (1, 1), (2, 0)
     
     if all(cell != ' ' for row in board for cell in row):
         return 'Tie'
     
     return None
-
-def make_ai_move():
-    row = random.randint(0, 2)
-    col = random.randint(0, 2)
-
-    if board[row][col] == ' ':
-        board[row][col] = 'O'
-        print("AI has made its move:")
-    else:
-        make_ai_move()
-
 
 def play_game():
     current_player = 'X'
@@ -73,7 +77,8 @@ def play_game():
             if winner == 'Tie':
                 print("It's a tie.")
             else:
-                print(f"Player {winner} wins.")
+                print(f"Player {current_player} wins.")
+                display_board(winner)
             break
         current_player = 'O' if current_player == 'X' else 'X'
 
